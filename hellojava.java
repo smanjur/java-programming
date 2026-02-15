ceecc37c79501da98c996b7b21cd14b0b3c8a930
smanjur <137135898+smanjur@users.noreply.github.com>
2026-02-15 10:15:02 +0530

    frist commit ,add hellojava program

---
+++ hellojava.java
@@ -0,0 +1,9 @@
+// This is my first Java program
+// It prints Hello World on screen
+
+
+public class hellojava {
+    public static void main(String [] args){
+        System.out.println("hello world");
+    }
+}
